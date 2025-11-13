a9f18b2754461d1d383aeeb79af9e3804fa0c60d
matheus-mmd <matheus_meneses2011@hotmail.com>
2025-11-12 23:08:24 -0300

    refactor(layout): ajustar estrutura e alinhamento do layout principal

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -45,10 +45,10 @@ export default function Layout() {
     <div className="flex h-screen bg-bg overflow-hidden">
       <Sidebar />
 
-      <div className="flex-1 overflow-y-auto">
+      <div className="flex-1 flex flex-col overflow-hidden">
         <Topbar user={user} balance={balance} />
 
-        <main className="p-6">
+        <main className="flex-1 overflow-y-auto p-6">
           <div className="max-w-7xl mx-auto">
             <Outlet />
           </div>
